bb1e6d0a218d019338f80522b1feeacbe7b7c37c
aerim <ars970207@naver.com>
2017-12-14 20:30:11 +0900

    FolderPermissionModel 추가

---
+++ src/main/java/com/dbdbdeep/websoft/models/FolderPermissionModel.java
@@ -0,0 +1,47 @@
+package com.dbdbdeep.websoft.models;
+
+public class FolderPermissionModel {
+    public static FolderPermissionModel get(){
+
+    }
+
+    public int getFolderId() {
+
+    }
+
+    public void setFolderId() {
+
+    }
+
+    public int getUserId() {
+
+    }
+
+    public void setUserId() {
+
+    }
+
+    public boolean isReadable() {
+        // isReadable =
+    }
+
+    public void setReadable(boolean isReadable) {
+
+    }
+
+    public boolean isWritable() {
+
+    }
+
+    public void setWritable(boolean isWritable) {
+
+    }
+
+    public boolean isPermittable() {
+
+    }
+
+    public void setPermittable(boolean isPermittable) {
+
+    }
+}
