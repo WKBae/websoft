abaa3ff97a6b1512d4ef088638f41907b72e1c32
aerim <ars970207@naver.com>
2017-12-16 21:31:04 +0900

    SearchServlet 추가

---
+++ src/main/java/com/dbdbdeep/websoft/servlet/SearchServlet.java
@@ -0,0 +1,58 @@
+package com.dbdbdeep.websoft.servlet;
+
+import com.dbdbdeep.websoft.models.FolderModel;
+import com.dbdbdeep.websoft.models.FileModel;
+import com.dbdbdeep.websoft.models.UserModel;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.Array;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.Queue;
+
+@WebServlet(name = "SearchServlet", urlPatterns = "/search/*")
+public class SearchServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        try {
+            UserModel user = (UserModel) request.getSession(true).getAttribute("user");
+            if (user == null) {
+                response.sendRedirect("/login");
+            }
+
+            String keyword = request.getParameter("keyword");
+            String path = request.getPathInfo();
+            String[] splitPath = path.split("/");
+
+            FolderModel rootFolder = FolderModel.getRoot(user);
+            FolderModel baseFolder = rootFolder.transverse(splitPath);
+            if(baseFolder == null) {
+                response.sendRedirect("/files");
+            }
+
+            ArrayList<FolderModel> sFolders = new ArrayList<>();
+            ArrayList<FileModel> sFiles = new ArrayList<>();
+            Queue<FolderModel> folders = new LinkedList<>();
+            folders.add(baseFolder);
+
+            FolderModel folder;
+            while((folder = folders.poll()) != null) {
+                Collections.addAll(sFiles, folder.searchFiles(keyword));
+                Collections.addAll(sFolders, folder.searchFolders(keyword));
+                Collections.addAll(folders, folder.getFolders());
+            }
+
+            request.setAttribute("files", sFiles);
+            request.setAttribute("folders", sFolders);
+            request.getRequestDispatcher("WEB-INF/jsp/search.jsp").forward(request, response);
+        } catch(SQLException e) {
+            throw new IOException(e);
+        }
+    }
+}
