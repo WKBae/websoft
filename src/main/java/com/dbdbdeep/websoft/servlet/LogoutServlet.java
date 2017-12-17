b02159c58e78b1e3bc393a22ce57e3344395a7ea
Chanho Park <chanho412@naver.com>
2017-12-17 13:54:32 +0900

    로그아웃 서블릿 생성

--- src/main/java/com/dbdbdeep/websoft/servlet/LogoutServlet.java
+++ src/main/java/com/dbdbdeep/websoft/servlet/LogoutServlet.java
@@ -17,8 +17,8 @@ public class LogoutServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         HttpSession session = request.getSession(); //현재 세션 리턴
 
-        session.removeAttribute("user");
+        session.removeAttribute("user"); // user 지우기
 
-        response.sendRedirect("/login");
+        response.sendRedirect("/login"); //로그인 페이지로 넘어감
     }
 }
