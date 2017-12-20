6f25e322d55b14833c9025c0e3391093ebd84215
aerim <ars970207@naver.com>
2017-12-21 02:39:07 +0900

    doGet 추가

--- src/main/java/com/dbdbdeep/websoft/servlet/SharedFolderServlet.java
+++ src/main/java/com/dbdbdeep/websoft/servlet/SharedFolderServlet.java
@@ -1,8 +1,11 @@
 package com.dbdbdeep.websoft.servlet;
 
+import com.dbdbdeep.websoft.models.FileModel;
 import com.dbdbdeep.websoft.models.FolderModel;
 import com.dbdbdeep.websoft.models.FolderPermissionModel;
 import com.dbdbdeep.websoft.models.UserModel;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -10,6 +13,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.sql.SQLException;
 import java.util.Arrays;
 
@@ -89,6 +93,57 @@ public class SharedFolderServlet extends HttpServlet {
 				return;
 			}
 		}catch (SQLException e){
+			throw new IOException(e);
+		}
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		try {
+			UserModel user = (UserModel) request.getSession(true).getAttribute("user");
+			if (user == null) {
+				response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
+				return;
+			}
+
+			String path = request.getPathInfo();
+			if (path == null) path = "/";
+			String[] splitPath = path.split("/");
+
+			int rootId = Integer.parseInt(splitPath[0]);
+			FolderModel rootFolder = FolderModel.get(rootId);
+			if(rootFolder == null) {
+				response.sendError(HttpServletResponse.SC_NOT_FOUND);
+			}
+
+			FolderModel baseFolder = rootFolder.transverse(splitPath);
+			if (baseFolder == null) {
+				response.sendError(HttpServletResponse.SC_NOT_FOUND);
+				return;
+			}
+
+			FolderPermissionModel baseFolderPermission = FolderPermissionModel.get(baseFolder, user);
+			if(baseFolderPermission == null) {
+				response.sendError(HttpServletResponse.SC_FORBIDDEN);
+			}
+
+			JSONArray folderList = new JSONArray();
+			JSONArray fileList = new JSONArray();
+
+			for (FolderModel folder : baseFolder.getFolders()) {
+				folderList.add(folder.getName());
+			}
+			for (FileModel file : baseFolder.getFiles()) {
+				fileList.add(file.getName());
+			}
+
+			JSONObject obj = new JSONObject();
+			obj.put("files", fileList);
+			obj.put("folders", folderList);
+
+			response.setContentType("application/json");
+			PrintWriter writer = response.getWriter();
+			writer.print(obj);
+		} catch (SQLException e) {
 			throw new IOException(e);
 		}
 	}
