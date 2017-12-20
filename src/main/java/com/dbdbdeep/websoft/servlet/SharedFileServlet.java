d1af7e3bae0c4bad8fe4f398ed9c2ffc07b74604
aerim <ars970207@naver.com>
2017-12-20 21:38:13 +0900

    공유받은 폴더, 파일 복사,이동,삭제

--- src/main/java/com/dbdbdeep/websoft/servlet/SharedFileServlet.java
+++ src/main/java/com/dbdbdeep/websoft/servlet/SharedFileServlet.java
@@ -16,7 +16,60 @@ import java.util.Arrays;
 
 @WebServlet(name = "SharedFileServlet")
 public class SharedFileServlet extends HttpServlet {
-	protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {  //파일 복사, 이동
+		try {
+			UserModel user = (UserModel) request.getSession(true).getAttribute("user");
+			if (user == null) {
+				response.sendRedirect("/login");
+				return;
+			}
+
+			String path = request.getPathInfo();
+			if (path == null) path = "/";
+			String[] splitPath = path.split("/");
+
+			int rootId = Integer.parseInt(splitPath[0]);
+			FolderModel rootFolder = FolderModel.get(rootId);
+			if(rootFolder == null) {
+				response.sendError(HttpServletResponse.SC_NOT_FOUND);
+			}
+			FolderPermissionModel folderPermission = FolderPermissionModel.get(rootFolder, user);
+			if(folderPermission == null) {  // 사용자가 rootFolder에 권한이 있는지 확인
+				response.sendError(HttpServletResponse.SC_NOT_FOUND);
+			}
+
+			FolderModel baseFolder = rootFolder.transverse(Arrays.copyOf(splitPath, splitPath.length - 1));
+			if (baseFolder == null) {
+				response.sendError(HttpServletResponse.SC_NOT_FOUND);
+				return;
+			}
+
+			FileModel target = baseFolder.getFile(splitPath[splitPath.length - 1]);
+			String to = request.getParameter("to");
+			String type = request.getParameter("type");//move, copy
+			String[] splitTo = to.split("/");
+			FolderModel toFolder = rootFolder.transverse(Arrays.copyOf(splitTo, splitTo.length - 1));
+			if(toFolder == null){
+				response.sendError(HttpServletResponse.SC_BAD_REQUEST);
+				return;
+			}
+
+			if("copy".equals(type)){
+				target.clone(toFolder);
+			}
+			else if("move".equals(type)){
+				target.setParent(toFolder);
+			}
+			else{
+				response.sendError(HttpServletResponse.SC_BAD_REQUEST);
+				return;
+			}
+		}catch (SQLException e){
+			throw new IOException(e);
+		}
+	}
+
+	protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {  //파일 삭제
 		try {
 			UserModel user = (UserModel) request.getSession(true).getAttribute("user");
 
